a64d51b9b44cf204e5c8b6d87b46c9e251d6543f
shenshan2011 <dana712.sesatism@gmail.com>
2025-03-08 20:53:44 +0700

    tugas

---
+++ quiz/part1/tg-2.js
@@ -0,0 +1,66 @@
+let tanggal = 12;
+let bulan = 3 ;
+let tahun = 2001;
+
+switch (bulan) {
+
+    case 1: namabulan = "januari" ;
+    
+    break;
+
+    case 2: namabulan = "februari" ;
+    
+    break;
+    
+    case 3: namabulan = "maret" ;
+    
+    break;
+
+    case 4: namabulan = "april" ;
+    
+    break;
+
+    case 5: namabulan = "mei" ;
+    
+    break;
+
+    case 6: namabulan = "juni" ;
+    
+    break;
+
+    case 7: namabulan = "juli" ;
+    
+    break;
+
+    case 8: namabulan = "agustus" ;
+    
+    break;
+
+    case 9: namabulan = "september" ;
+    
+    break;
+
+    case 10: namabulan = "oktober" ;
+    
+    break;
+
+    case 11: namabulan = "november" ;
+    
+    break;
+
+    case 12: namabulan = "desember" ;
+    
+    break;
+
+   
+    
+    default:
+        console.log(typeof undefined);
+        break;
+    
+    
+} 
+
+{console.log(`tanggal: ${tanggal} ${namabulan} ${tahun}`)
+
+}
