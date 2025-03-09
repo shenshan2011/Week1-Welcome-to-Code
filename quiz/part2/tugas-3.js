73d1275af7c68110f409a0b23ec13e824f853e26
shenshan2011 <dana712.sesatism@gmail.com>
2025-03-09 10:48:52 +0700

    tugas3

---
+++ quiz/part2/tugas-3.js
@@ -0,0 +1,18 @@
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+//          012345678901234567890
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word.substring(0, 3);
+let towWord = word.substring(4, 14);
+let rhWord = word.substring(15, 17);
+let forWord = word.substring(18, 20);
+let fivWord = word.substring(21, 25);
+
+{console.log('First Word: ' + exampleFirstWord);
+    console.log('Second Word: ' + towWord);
+    console.log('Third Word: ' + rhWord);
+    console.log('fourth Word: ' + forWord);
+    console.log('fifth Word: ' + fivWord);
+    }
+
+    
